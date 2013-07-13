c50c9d046a1ba94fc402fe7dd0e5efd682a8614b
vicky11z <vicky11zhang@yahoo.com>
2013-07-13 05:57:21 +0000

    updated web.js 2

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var index = fs.readFileSync("index.html");
+
+var buf = new Buffer(index);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
